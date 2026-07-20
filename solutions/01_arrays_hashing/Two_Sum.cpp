28d86925a4b9d4bc4c01e161b2e28c7fa30ab3cb
hausmannmike <hausmannmike@yahoo.com>
2026-07-20 19:00:38 +0200

    Add Two Sum solution

---
+++ solutions/01_arrays_hashing/Two_Sum.cpp
@@ -0,0 +1,22 @@
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+
+        unordered_map<int, int> seen;
+
+        for (int i = 0; i < nums.size(); i++) {
+            int need = target - nums[i];
+            if (seen.count(need)) {
+                return {seen[need], i};
+            }
+            seen[nums[i]] = i;
+        }
+        
+        return {};
+    }
+};
